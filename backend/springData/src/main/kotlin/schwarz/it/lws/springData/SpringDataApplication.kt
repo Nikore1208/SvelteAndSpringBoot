3859e81ab257d3a693177b644e652b9208580dc5
reicheln <niko.reichel@stackit.cloud>
2024-12-11 12:22:12 +0100

    init

---
+++ backend/springData/src/main/kotlin/schwarz/it/lws/springData/SpringDataApplication.kt
@@ -0,0 +1,11 @@
+package schwarz.it.lws.springData
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDataApplication>(*args)
+}
---
+++ backend/src/main/kotlin/schwarz/it/lws/repository/WeatherRepository.kt
@@ -0,0 +1,16 @@
+package schwarz.it.lws.repository
+
+import schwarz.it.lws.model.WeatherData
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
+
+@Repository
+interface WeatherRepository : JpaRepository<WeatherData, Long> {
+
+    /*fun findByCityAndForecastDataBetween(
+        city: String,
+        startDate: LocalDateTime,
+        endDate: LocalDateTime
+    ): List<WeatherData>*/
+}
