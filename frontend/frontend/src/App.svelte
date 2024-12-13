46e03567ebd611f9bb72040ef64fa189ca48f6a0
abdyraeva <nurzhanemilbekkz@gmail.com>
2024-12-13 15:28:20 +0100

    add api

--- frontend/frontend/src/App.svelte
+++ frontend/frontend/src/App.svelte
@@ -1,18 +1,83 @@
 <script>
-  import Weather from './lib/Weather.svelte';
+  import WeatherCard from './lib/WeatherCard.svelte';
+
+  let city = '';
+  let weatherData = null;
+  let selectedDay = 0;
+
+  async function searchWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=9eb6056c21bd7f742bff107b5794c961`);
+    if (!response.ok) {
+      alert('City not found or API error');
+      return;
+    }
+    const data = await response.json();
+    weatherData = data.list.filter((_, index) => index % 8 === 0);
+    console.log(weatherData)
+  }
+
+  function selectDay(index) {
+    selectedDay = index;
+  }
 </script>
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 600px;
-    margin: auto;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  .search {
+    display: flex;
+    margin-bottom: 20px;
+  }
+  input {
+    flex-grow: 1;
+    padding: 10px;
+  }
+  button {
+    padding: 10px 20px;
+  }
+  .weather-container {
+    display: flex;
+    justify-content: space-between;
+  }
+  .forecast {
+    display: flex;
+    flex-direction: column;
+  }
+  .forecast button {
+    margin-bottom: 10px;
+  }
+  .active {
+    background-color: #007bff;
+    color: white;
   }
 </style>
 
 <main>
-  <h1>Wetter Informations News</h1>
-  <Weather />
-</main>
+  <h1>Weather Forecast</h1>
 
+  <div class="search">
+    <input bind:value={city} placeholder="Enter city" />
+    <button on:click={searchWeather}>Search</button>
+  </div>
+
+
+  {#if weatherData}
+    <div class="weather-container">
+      <WeatherCard weather={weatherData[selectedDay]} />
+
+      <div class="forecast">
+        {#each weatherData as day, index}
+          <button class:active={index === selectedDay} on:click={() => selectDay(index)}>
+            {new Date(day.dt * 1000).toLocaleDateString()}
+          </button>
+        {/each}
+      </div>
+    </div>
+  {/if}
+  <div>
+    <img src="./assets/clear-day.svg" alt="clear-day">
+  </div>
+</main>
---
+++ frontend/frontend/src/lib/WeatherCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let weather;
+</script>
+
+<style>
+    .weather-card {
+        border: 1px solid #ddd;
+        padding: 20px;
+        border-radius: 5px;
+        margin-bottom: 20px;
+    }
+</style>
+
+<div class="weather-card">
+    <h2>{new Date(weather.dt * 1000).toLocaleDateString()}</h2>
+    <p>Temperature: {weather.main.temp}°C</p>
+    <p>Min temperature: {weather.main.temp_min}°C</p>
+    <p>Max temperature: {weather.main.temp_max}°C</p>
+    <p>Humidity: {weather.main.humidity}%</p>
+    <p>Air Pressure: {weather.main.pressure} hPa</p>
+    <p>Description: {weather.weather[0].description}</p>
+</div>
+
