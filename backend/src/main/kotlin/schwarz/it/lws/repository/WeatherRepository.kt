955d2fc056a5c30c0ef70dc9357e97ee49ce7332
reicheln <niko.reichel@stackit.cloud>
2024-12-13 08:44:38 +0100

    init

--- backend/src/main/kotlin/schwarz/it/lws/repository/WeatherRepository.kt
+++ backend/src/main/kotlin/schwarz/it/lws/repository/WeatherRepository.kt
@@ -2,13 +2,14 @@ package schwarz.it.lws.repository
 
 import schwarz.it.lws.model.WeatherData
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
 interface WeatherRepository : JpaRepository<WeatherData, Long> {
 
-    fun findByCity(city: String): List<WeatherData>
+    fun findTopByCityOrderByForecastDateDesc(city: String): WeatherData
 
 
     /*fun findByCityAndForecastDataBetween(
